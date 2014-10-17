8c162d86a65ec005f6efe3490f90a8aa8375e4c3
Bruce Lefebvre <blefebvr@adobe.com>
2014-10-16 20:06:08 -0400

    Corrected customize script to search depth first

--- customize-app.sh
+++ customize-app.sh
@@ -7,10 +7,10 @@ if [ $# -ne 2 ]
     exit 1
 fi
 
-find . -name brand_name_placeholder -type d -execdir mv {} $1 \;
-find . -name app_name_placeholder -type d -execdir mv {} $2 \;
+find . -name brand_name_placeholder -type d -depth -execdir mv {} $1 \;
+find . -name app_name_placeholder -type d -depth -execdir mv {} $2 \;
 
-find . -type f \( -name '*.xml' -o -name '*.jsp' -o -name index.html -o -name config.json \) -exec sed -i '' s/brand_name_placeholder/$1/g {} \;
-find . -type f \( -name '*.xml' -o -name '*.jsp' -o -name index.html -o -name config.json \) -exec sed -i '' s/app_name_placeholder/$2/g {} \;
+find . -type f \( -name '*.xml' -o -name '*.jsp' -o -name index.html -o -name config.json \) -depth -exec sed -i '' s/brand_name_placeholder/$1/g {} \;
+find . -type f \( -name '*.xml' -o -name '*.jsp' -o -name index.html -o -name config.json \) -depth -exec sed -i '' s/app_name_placeholder/$2/g {} \;
 
 echo "Finished customizing app with $1 brand name and $2 app name."
